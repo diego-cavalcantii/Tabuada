5bf652f2873d3a71b7f989b3912c085fe8827045
diego <diego.scavalcanti@gmail.com>
2023-10-26 15:18:54 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tabuada
+Projeto Python de tabuada
+
---
+++ tabuada.py
@@ -0,0 +1,12 @@
+'''
+	Elaborar um programa em Python que efetue o cálculo de uma tabuada de um número qualquer e armazene os resultados em uma lista A para 11 elementos. Apresentar os valores armazenados na lista.
+'''
+
+listaA = []
+num = int(input("Digite um número para exibir a tabuada : "))
+for i in range(1,11):
+    listaA.append(i * num)
+
+print("\n==== Tabuada ====\n")
+for i in range(10):
+    print(f"{num} x {i + 1} = {listaA[i]}")
